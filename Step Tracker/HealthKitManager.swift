e46f679c44af1c56ebdef7030da8b5a84b6bfef2
owhu <oliverhu.oh@gmail.com>
2025-04-24 15:41:53 -0700

    mock data on simulator
    Intentionally leaving commented code for future use.  Will delete later.

--- Step Tracker/HealthKitManager.swift
+++ Step Tracker/HealthKitManager.swift
@@ -15,4 +15,25 @@ import Observation
     
     let types: Set = [HKQuantityType(.stepCount), HKQuantityType(.bodyMass)]
     
+//    func addSimulatorSample() async {
+//        var mockSamples: [HKQuantitySample] = []
+//        
+//        for i in 0..<28 {
+//            let stepQuantity = HKQuantity(unit: .count(), doubleValue: .random(in: 4000...20000))
+//            let weightQuantity = HKQuantity(unit: .pound(), doubleValue: .random(in: 160 + Double(i/3)...165 + Double(i/3)))
+//            
+//            let startDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
+//            let endDate = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
+////            let endDate = Calendar.current.date(byAdding: .second, value: 1, to: startDate)!
+//            
+//            let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepQuantity, start: startDate, end: endDate)
+//            let weightSample = HKQuantitySample(type: HKQuantityType(.bodyMass), quantity: weightQuantity, start: startDate, end: endDate)
+//            
+//            mockSamples.append(stepSample)
+//            mockSamples.append(weightSample)
+//        }
+//        
+//        try! await store.save(mockSamples)
+//        print("✅ Dummy date sent up.")
+//    }
 }
