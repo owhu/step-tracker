65520d97dd0ac198582d083d7d44dc5310fc4dea
owhu <oliverhu.oh@gmail.com>
2025-04-29 12:01:03 -0700

    fetching steps and weight

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -68,16 +68,16 @@ struct DashboardView: View {
                     .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
                     
                     VStack(alignment: .leading) {
-                            VStack(alignment: .leading) {
-                                Label("Averages", systemImage: "calendar")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.pink)
-                                
-                                Text("Last 28 days")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-                            .padding(.bottom, 12)
+                        VStack(alignment: .leading) {
+                            Label("Averages", systemImage: "calendar")
+                                .font(.title3.bold())
+                                .foregroundStyle(.pink)
+                            
+                            Text("Last 28 days")
+                                .font(.caption)
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom, 12)
                         
                         RoundedRectangle(cornerRadius: 12)
                             .foregroundStyle(.secondary)
